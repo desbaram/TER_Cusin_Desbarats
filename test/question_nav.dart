a8bc0c5d605384b2b9369d57eb78e4504199acd7
Marck-Edward <edwardmarck@gmail.com>
2021-05-26 14:38:52 +0200

    question and ans test using arrays. first git test

--- test/question_nav.dart
+++ test/question_nav.dart
@@ -4,8 +4,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:compagnon_virtuel/home.dart';
 import 'package:mockito/mockito.dart';
+import 'dart:math';
 
 class MockNavigatorObserver extends Mock implements NavigatorObserver {}
+
+String getRandomElement<String>(List<String> list) {
+  final random = new Random();
+  var i = random.nextInt(list.length);
+  return list[i];
+}
+
+var list = [
+  "better than yesterday",
+  "i will take a walk today with my nurse",
+  "am feeling great but will be visiting my doctor tomorrow",
+  "fit as a fiddle",
+  "healthy and good",
+  "feeling to take a stroll and go shopping",
+];
 
 void main() {
   testWidgets('navigation of question button', (WidgetTester tester) async {
@@ -41,15 +57,13 @@ void main() {
     );
     expect(find.byType(TextFormField), findsOneWidget);
     // await tester.pumpAndSettle();
+    var element = getRandomElement(list);
+    print(element);
+    await tester.enterText(find.byType(TextFormField), element);
+    expect(find.text(element), findsOneWidget);
     var send = find.text("Envoyer");
     await tester.pump(Duration(seconds: 2));
     await tester.tap(send, warnIfMissed: false);
-
-    await tester.enterText(find.byType(TextFormField), 'Porte bien aujourdhui');
-    expect(find.text('Porte bien aujourdhui'), findsOneWidget);
-    var send1 = find.text("Envoyer");
-    await tester.pump(Duration(seconds: 2));
-    await tester.tap(send1, warnIfMissed: false);
     await tester.pumpAndSettle();
   });
 }
